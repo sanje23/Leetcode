b1d5236fe08a6032a61c82062e3248695de6e6b0
SANJEEVI R <161220197+sanje23@users.noreply.github.com>
2025-04-18 22:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (57.49%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,17 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        index=-1
         n=len(nums)
+        index=-1
         for i in range(n-2,-1,-1):
             if nums[i]<nums[i+1]:
                 index=i
                 break
         if index==-1:
-            nums.reverse()
-            return
+            return nums.reverse()
         for i in range(n-1,index-1,-1):
             if nums[i]>nums[index]:
-                nums[index],nums[i]=nums[i],nums[index]
+                temp=nums[index]
+                nums[index]=nums[i]
+                nums[i]=temp
                 break
         nums[index+1:]=reversed(nums[index+1:])
