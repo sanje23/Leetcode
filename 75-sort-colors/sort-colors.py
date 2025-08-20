7d6ac4fd2ed48de73fbc83c61d2e758d4e3e4336
SANJEEVI R <161220197+sanje23@users.noreply.github.com>
2025-08-20 18:56:05 +0530

    Time: 3 ms (13.00%) | Memory: 17.7 MB (83.55%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,10 +1,10 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         freq={0:0,1:0,2:0}
+        q=0
         for i in nums:
             freq[i]+=1
-        q=0
-        for i in range(q,freq[0]):
+        for i in range(0,freq[0]):
             nums[i]=0
             q+=1
         for i in range(q,freq[1]+q):
@@ -12,3 +12,4 @@ class Solution:
             q+=1
         for i in range(q,len(nums)):
             nums[i]=2
+        
